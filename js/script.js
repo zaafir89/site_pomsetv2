e6e62984f6450eb5038a4c0169a088ddc0bec826
jbh225 <jb.hallassou@gmail.com>
2017-08-04 17:47:47 +0200

    premier commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//oie
+
+function openOie() {
+    document.getElementById("oie").style.width = "50%";
+
+}
+function closeOie() {
+    document.getElementById("oie").style.width = "0";
+}
+
+//areas
+
+function openAreas() {
+    document.getElementById("areas").style.width = "50%";
+
+}
+function closeAreas() {
+    document.getElementById("areas").style.width = "0";
+}
+
+//adp
+
+function openAdp() {
+    document.getElementById("adp").style.width = "50%";
+
+}
+function closeAdp() {
+    document.getElementById("adp").style.width = "0";
+}
+
+//aviva
+
+function openAviva() {
+    document.getElementById("aviva").style.width = "50%";
+
+}
+function closeAviva() {
+    document.getElementById("aviva").style.width = "0";
+}
