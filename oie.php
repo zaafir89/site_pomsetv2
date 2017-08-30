1a925a3dffc063a973edcced9c514dfa90b134bc
jbh225 <jb.hallassou@gmail.com>
2017-08-30 17:34:41 +0200

    ajout marge page oie

--- oie.php
+++ oie.php
@@ -1,145 +1,155 @@
 <?php include'layout.php'; ?>
 
 <div class="content-droit">
+  <div class="container-">
 
 
-  <p class=" premier-titre">une idée 360°/ OIE rapport annuel 2017</p>
+    <p class="premier-titre">une idée 360°/ OIE rapport annuel 2017</p>
 
-  <a href="index.php"<button type="submit" class="btn3 btn-envoyer right">fermer</button></a>
-  <div class="container">
+    <a href="index.php"<button type="submit" class="btn3 btn-envoyer right">fermer</button></a>
+    <div class="container">
 
-    <div class="row">
-      <div class="col-xs-offset-6 col-xs-6">
-        <img src="img/pomset/default/g.jpg" class="img-responsive" alt="work"/>
-        <h2 class="trait text-noir">Titre mot <strong>double</strong> ou pas.</h2>
-        <h3 class="text-noir"><strong>Le challenge</strong></h3>
-        <p class="text-noir">
-          Customers love our block-based approach to template building,
-          it makes assembling beautiful pages fast and enjoyable, leaving
-          more time to craft your perfect layout. love our block-based
-          approach to template building, it makes assembling beautiful pages
-          fast and enjoyable, leaving more time to craft your perfect layout.
-        </p>
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-6">
+          <img src="img/pomset/default/g.jpg" class="img-responsive" alt="work"/>
+          <h2 class="trait text-noir">Titre mot <strong>double</strong> ou pas.</h2>
+          <h3 class="text-noir ecart1"><strong>Le challenge</strong></h3>
+          <p class="text-noir">
+            Customers love our block-based approach to template building,
+            it makes assembling beautiful pages fast and enjoyable, leaving
+            more time to craft your perfect layout. love our block-based
+            approach to template building, it makes assembling beautiful pages
+            fast and enjoyable, leaving more time to craft your perfect layout.
+          </p>
 
-        <h3><strong>Nos solutions</strong></h3>
-        <ul>
-          <li>RA 56 pages PDF interactif</li>
-          <li>Dépliant 8 pages / 4 volets</li>
-          <li>Video 1min30</li>
-          <li>Web flyer</li>
-          <li>Un mini site</li>
-        </ul>
-      </div>
-
-    </div>
-
-    <div class="row">
-      <div class="col-xs-offset-6 col-xs-6">
-        <div class="col-sm-6 col-xs-12">
-          <img src="img/pomset/default/test.jpg" class="img-responsive" alt="work"/>
+          <h3 class="ecart1"><strong>Nos solutions</strong></h3>
+          <ul>
+            <li>RA 56 pages PDF interactif</li>
+            <li>Dépliant 8 pages / 4 volets</li>
+            <li>Video 1min30</li>
+            <li>Web flyer</li>
+            <li>Un mini site</li>
+          </ul>
         </div>
 
-        <div class="col-sm-6 col-xs-12">
-          <img src="img/pomset/default/test.jpg" class="img-responsive" alt="work"/>
-        </div>
       </div>
 
-    </div>
-    <span class="ecart"></span>
-
-    <div class="row">
-      <div class="col-xs-offset-6 col-xs-6">
-        <img src="img/pomset/default/g.jpg" class="img-responsive ecart" alt="work"/>
-      </div>
-
-    </div>
-
-  </div>
-  <!--fin du container-->
-  <div class="container bg">
-    <div class="row">
-      <div class="col-xs-offset-6 col-xs-6">
-        <img src="img/pomset/default/h.jpg" class="img-responsive" alt="work"/>
-      </div>
-    </div>
-  </div>
-
-  <div class="container bg2">
-    <div class="row">
-      <div class="col-xs-offset-6 col-xs-6">
-        <img src="img/pomset/default/h.jpg" class="img-responsive" alt="work"/>
-      </div>
-    </div>
-  </div>
-
-  <div class="container">
-    <div class="col-xs-offset-6 col-xs-6 ecart">
-      <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/131777654?title=0&byline=0&portrait=0"></iframe>
-      </div>
-
-
-    </div>
-
-  </div>
-  <div class="col-xs-offset-6 col-xs-3">
-    <span class="separation"></span>
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-offset-6 col-xs-6">
-        <div class="col-sm-6 col-xs-12">
-          <h3 class="text-noir"><strong>Les chiffres</strong></h3>
-          <h2 class="text-noir"><strong><span class="textcolor">DOLOR</span></strong></h2>
-          <p class="text-noir">Lorem ipsum sit mata</p>
-        </div>
-
-
-        <div class="col-sm-6 col-xs-12">
-          <h3 class="texte"><strong>test</strong></h3>
-          <h2 class="text-noir"><strong><span class="textcolor">45 MIN</span></strong></h2>
-          <p class="text-noir">Lorem ipsum sit mata</p>
-        </div>
-      </div>
-
-    </div>
-    <div class="col-xs-offset-6 col-xs-3">
-      <span class="separation"></span>
-    </div>
-
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-offset-6 col-xs-6">
-        <div>
-          <h2 class="text-noir"><strong>Cela vous</strong> <em>intéresse?</em><br> Contactez nous et nous reviendrons vers vous très vite!</h2>
-        </div>
-
-        <div class="row">
-          <div class="col-xs-3">
-            <p class="text-noir">
-              <strong>Addresse :</strong> <br>
-              24 Rue Carle Hebert, <br>
-              92400 Courbevoie <br>
-            </p>
-            <p class="text-noir"><strong>Téléphone: </strong><br>  (+33) 1 47 58 14 31<br> <br></p>
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-6">
+          <div class="col-sm-6 col-xs-12">
+            <img src="img/pomset/default/test.jpg" class="img-responsive" alt="work"/>
           </div>
-          <div class="col-xs-9">
-            <form class="form-inline">
-              <div class="form-group">
-                <input type="text" class="form-control" id="exampleInputName2" placeholder="Nom">
-                <input type="email" class="form-control email" id="exampleInputEmail2" placeholder="Email">
-              </div>
-              <div class="form-group">
-                <textarea class="form-control message" rows="6" cols="60" placeholder="Message"></textarea>
-                <button type="submit" class="btn btn-envoyer right">envoyer</button>
-              </div>
 
-            </form>
+          <div class="col-sm-6 col-xs-12">
+            <img src="img/pomset/default/test.jpg" class="img-responsive" alt="work"/>
           </div>
         </div>
 
       </div>
+      <span class="ecart"></span>
+
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-6">
+          <img src="img/pomset/default/g.jpg" class="img-responsive ecart" alt="work"/>
+        </div>
+
+      </div>
+
+    </div>
+    <!--fin du container-->
+    <div class="container bg">
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-6">
+          <img src="img/pomset/default/h.jpg" class="img-responsive" alt="work"/>
+        </div>
+      </div>
+    </div>
+
+    <div class="container bg2">
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-6">
+          <img src="img/pomset/default/h.jpg" class="img-responsive" alt="work"/>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="col-xs-offset-6 col-xs-6 ecart">
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/131777654?title=0&byline=0&portrait=0"></iframe>
+        </div>
+
+
+      </div>
+
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-3">
+          <span class="separation"></span>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-6">
+          <div class="col-sm-6 col-xs-12">
+            <h3 class="text-noir"><strong>Les chiffres</strong></h3>
+            <h2 class="text-noir"><strong><span class="textcolor">DOLOR</span></strong></h2>
+            <p class="text-noir">Lorem ipsum sit mata</p>
+          </div>
+
+
+          <div class="col-sm-6 col-xs-12">
+            <h3 class="texte"><strong>test</strong></h3>
+            <h2 class="text-noir"><strong><span class="textcolor">45 MIN</span></strong></h2>
+            <p class="text-noir">Lorem ipsum sit mata</p>
+          </div>
+        </div>
+
+      </div>
+      <div class="col-xs-offset-6 col-xs-3">
+        <span class="separation"></span>
+      </div>
+
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-offset-6 col-xs-6">
+          <div>
+            <h2 class="text-noir"><strong>Cela vous</strong> <em>intéresse?</em><br> Contactez nous et nous reviendrons vers vous très vite!</h2>
+          </div>
+
+          <div class="row">
+            <div class="col-xs-3">
+              <p class="text-noir">
+                <strong>Addresse :</strong> <br>
+                24 Rue Carle Hebert, <br>
+                92400 Courbevoie <br>
+              </p>
+              <p class="text-noir"><strong>Téléphone: </strong><br>  (+33) 1 47 58 14 31<br> <br></p>
+            </div>
+            <div class="col-xs-9">
+              <form class="form-inline">
+                <div class="form-group">
+                  <input type="text" class="form-control" id="exampleInputName2" placeholder="Nom">
+                  <input type="email" class="form-control email" id="exampleInputEmail2" placeholder="Email">
+                </div>
+                <div class="form-group">
+                  <textarea class="form-control message" rows="6" cols="60" placeholder="Message"></textarea>
+                  <button type="submit" class="btn btn-envoyer right">envoyer</button>
+                </div>
+
+              </form>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="col-xs-offset-6 col-xs-3 ecart1">
+        <p class="text-noir">© 2017 Made with love by Pomset</p>
+      </div>
+
     </div>
   </div>
 </div>
