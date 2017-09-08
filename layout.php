e1e134f6d0a6f1f7fbb642cf75469607c5bf1012
jbh225 <jb.hallassou@gmail.com>
2017-09-08 15:14:35 +0200

    ajustementbouton gauche

--- layout.php
+++ layout.php
@@ -25,7 +25,7 @@
 
       <div class="row">
         <div class="col-lg-10 col-lg-offset-1 col-xs-offset-1 col-xs-10">
-          <h3 class="pacifico texte font">Tout commence par une idée...</h3>
+          <h3 class="pacifico texte font1">Tout commence par une idée...</h3>
         </div>
         <div class="col-lg-10 col-lg-offset-1 col-xs-offset-1 col-xs-12  deuxieme-titre deuxieme-titre1">
           <h3 class="texte font"><strong>parce qu'une bonne idée <br/>
@@ -36,20 +36,20 @@
 
         <div class="row">
           <div class="col-lg-4 col-xs-offset-1 col-md-6 col-xs-12">
-            <a href="oie.php"><button type="button" class="btn btn2">une idée 360</button></a>
-            <a href="areas.php"><button type="button" class="btn btn5">une idée performante</button></a>
+            <a href="oie.php"><button type="button" class="btn btn2"><span class="font2">une idée 360</span></button></a>
+            <a href="areas.php"><button type="button" class="btn btn5"><span class="font2">une idée performante</span></button></a>
           </div>
           <div class="col-lg-4 col-xs-offset-1 col-md-6 col-xs-12">
-            <a href="adp.php"><button type="button" class="btn btn2 btn5">une idée lab</button></a>
-            <a href="aviva.php"><button type="button" class="btn btn5 ">une idée génératrice</button></a>
+            <a href="adp.php"><button type="button" class="btn btn2 btn5"><span class="font2">une idée lab</span></button></a>
+            <a href="aviva.php"><button type="button" class="btn btn5 "><span class="font2">une idée génératrice</span></button></a>
           </div>
         </div>
 
-        <div class="col-lg-3 col-lg-offset-1 col-xs-3 col-xs-offset-1 quatrieme-titre quatrieme-titre-bis">
+        <div class="col-lg-4 col-lg-offset-1 col-xs-3 col-xs-offset-1 quatrieme-titre quatrieme-titre-bis">
 
-          <p class="texte size1">On garde le contact</p>
+          <p class="texte size2">On garde le contact</p>
         </div>
-        <div class="col-lg-3 col-xs-3 quatrieme-titre">
+        <div class="col-lg-2 col-xs-3 quatrieme-titre">
           <span class="social"><a href="https://www.facebook.com/PomsetAgence/" target="_blank"><img src="img/pomset/picto/fb.png" alt="picto document"></a></span>
           &nbsp;&nbsp;&nbsp;&nbsp;
           <span class="social"><a href="http://www.linkedin.com/company/pomset-agence-de-communication-globale" target="_blank"><img src="img/pomset/picto/link3.png" alt="picto document" class="align-picto3"></a></span>
