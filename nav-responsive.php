b06430f689b5470866ae6e3bd104c82edee6d79d
jbh225 <jb.hallassou@gmail.com>
2017-09-29 12:14:59 +0200

    ajout modif css responsive pour menu

--- nav-responsive.php
+++ nav-responsive.php
@@ -1,14 +1,17 @@
 
-<head>
-  <link rel="stylesheet" href="/css/master.css">
-</head>
+
 <body>
+
+
   <div class="liens">
 
     <div class="row">
       <div class="col-lg-offset-6 col-lg-6 col-xs-12 marge4 marge5">
+
+
         <a class="lien" <?php if ($navResponsive == 'oie') { echo 'id="liensResponsive"';} ?> href="oie.php">une idée 360° </a>|
         <a class="lien" <?php if ($navResponsive == 'adp') { echo 'id="liensResponsive"';} ?>href="adp.php">une idée sur mesure </a>|
+
 
         <a class="lien" <?php if ($navResponsive == 'areas') { echo 'id="liensResponsive"';} ?> href="areas.php">une idée performante </a>|
         <a class="lien" <?php if ($navResponsive == 'aviva') { echo 'id="liensResponsive"';} ?> href="aviva.php">une idée génératrice </a>
@@ -17,4 +20,5 @@
 
     </div>
   </div>
+
 </body>
